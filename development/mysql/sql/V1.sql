a0b5719674ba3f084a69e91d393e6ad249081670
tomoya ishigaki <steinwand.tomo@gmail.com>
2022-04-16 16:51:26 +0900

    Add add index DLL in V1

--- development/mysql/sql/V1.sql
+++ development/mysql/sql/V1.sql
@@ -1,1 +1,4 @@
 alter table session add index session_idx_01 (value);
+alter table group_member add index group_member_idx_01 (user_id);
+alter table record_comment add index record_comment_idx_01 (linked_record_id);
+alter table record add index record_idx_01 (status);
