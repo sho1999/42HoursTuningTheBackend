23496dab6b32d861201932e81deb2cf597770e26
tomoya ishigaki <steinwand.tomo@gmail.com>
2022-04-16 18:03:35 +0900

    add index for sort

--- development/mysql/sql/V1.sql
+++ development/mysql/sql/V1.sql
@@ -2,3 +2,4 @@ alter table session add index session_idx_01 (value);
 alter table group_member add index group_member_idx_01 (user_id);
 alter table record_comment add index record_comment_idx_01 (linked_record_id);
 alter table record add index record_idx_01 (status);
+alter table record add index record_idx_02 (updated_at, record_id);
