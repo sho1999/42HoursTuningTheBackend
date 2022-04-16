a576c1a3a70ab1612787796a4e9159d1bfbf500d
tomoya ishigaki <steinwand.tomo@gmail.com>
2022-04-16 20:51:06 +0900

    add index. 1679

--- development/mysql/sql/V1.sql
+++ development/mysql/sql/V1.sql
@@ -4,5 +4,6 @@ alter table record_comment add index record_comment_idx_01 (linked_record_id);
 alter table record add index record_idx_01 (status);
 alter table record add index record_idx_02 (updated_at desc, record_id);
 alter table record add index record_idx_03 (created_by, status);
+alter table record add index record_idx_04 (category_id, application_group);
 alter table record_item_file add index record_item_file_idx_01 (linked_record_id);
 alter table record_item_file add index record_item_file_idx_02 (item_id);
